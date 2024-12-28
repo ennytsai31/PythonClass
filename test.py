746202fbe40f14ace83ed8b48d1a4cff6ca56e87
456 <123@example.com>
2024-12-28 13:38:20 +0800

    import v1

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+import sys  #載入sys套件
 print("Hello World")
 a = 1
 if a > 1:
     print ("OK")
 else:
     print ("ELSE")
+sys.exit(12)
 print("hi")
 b = a+1
 print("123")
---
+++ test1228.py
@@ -0,0 +1,4 @@
+import sys  #載入sys套件
+print("good")
+sys.exit(12)    #顯示errcode 12 
+print("hi")
